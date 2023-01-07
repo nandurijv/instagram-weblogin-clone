d17a5ae45e89625ba87ef2a53a8a146d78339260
Nanduri Jayant Vishnu <nandurijayant.vishnu2020@vitstudent.ac.in>
2023-01-07 15:18:35 +0530

    added loader

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             exact
             path="/"
             element={
-              count&&<Login/>
+              !count&&<Login/>
             }
           />
         </Routes>
